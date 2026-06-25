fd59338b3c9ff8466fd8ad7a9e3fe97fe13dc809
Lovelyn Pojas <lovelynpojas03@gmail.com>
2026-06-25 20:50:09 +0800

    Added SampleComponent and updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SampleComponent from './components/SampleComponent.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <SampleComponent />
 </template>
---
+++ src/components/SampleComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Sample Component</h1>
+  </div>
+</template>
